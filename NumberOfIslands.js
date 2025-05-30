c76ec9c3ef711125e0bf0b172afbecb7e109387f
JoyChudasama <joychudasama11@gmail.com>
2025-05-30 07:16:13 -0300

    Number of Islands

--- NumberOfIslands.js
+++ NumberOfIslands.js
@@ -22,35 +22,43 @@
 //   ]
 // Output: 4
 
+// Notes: 
+// We have to loop over each direction for each grid[row][col] to find the adjacent with boundry check and grid[r][c] being 1
+// We don't require Set() to keep track of what we have visited because we are marking each visited node as 0 and in our check we are only exploring node that is not 0  
+
 // O(r * c) time
 // O(r * c) space
 function numIslands(grid) {
-    const rows = grid.length;
-    const cols = grid[0].length;
+    const totalRows = grid.length;
+    const totalCols = grid[0].length;
     const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
     let islands = 0;
 
     const bfs = (r, c) => {
         const q = [];
         q.push([r, c]);
-        grid[r][c] = '0';
+        grid[r][c] = '0'; // Marking as seen
 
         while (q.length > 0) {
 
             const [row, col] = q.shift();
 
+            // Going in each direction to see if it is a valid node or not
             for (const [dr, dc] of directions) {
                 const nr = row + dr, nc = col + dc;
-                if (nr >= 0 && nc >= 0 && nr < rows && nc < cols && grid[nr][nc] === '1') {
+                
+                // Only explore further if indices are valid and cell is 1
+                if (nr >= 0 && nc >= 0 && nr < totalRows && nc < totalCols && grid[nr][nc] === '1') {
                     q.push([nr, nc]);
-                    grid[nr][nc] = '0';
+                    grid[nr][nc] = '0'; // Marking as seen
                 }
             }
         }
     }
 
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
+    // Run the exploration on each cell if value is 1
+    for (let r = 0; r < totalRows; r++) {
+        for (let c = 0; c < totalCols; c++) {
             if (grid[r][c] === '1') {
                 bfs(r, c);
                 islands++;
