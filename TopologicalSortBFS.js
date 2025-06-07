152f988111cd726308b6f5f46bac39bb6f0b2aca
JoyChudasama <joychudasama11@gmail.com>
2025-06-07 07:16:25 -0300

    Topological Sort - BFS/Kahn's Algorithm

--- TopologicalSortBFS.js
+++ TopologicalSortBFS.js
@@ -1,18 +1,37 @@
-function topologicalSort(jobs, deps) {
-    const adjList = new Map();
-    for (let i = 0; i < jobs.length; i++) adjList.set(jobs[i], []);
-    for (const [a, b] of deps) adjList.get(b).push(a);
+// Topological Sort - BFS/Kahn's Algorithm 
 
-    const indegrees = new Array(jobs.length + 1).fill(0);
-    for (const [a, b] of adjList) {
-        for (const i of b) {
-            indegrees[i] += 1;
+// It works by repeatedly removing nodes with zero indegree (i.e., no incoming edges), ensuring that all dependencies are resolved before a node is processed.
+
+// How It Works
+
+// Build the graph (adjacency list).
+// Calculate indegree (number of incoming edges) for each node.
+// Initialize a queue with all nodes that have 0 indegree.
+// While the queue is not empty:
+// Remove a node from the queue.
+// Add it to the topological order.
+// Decrease the indegree of its neighbors by 1.
+// If any neighbor’s indegree becomes 0, add it to the queue.
+// If the result contains all nodes, return it. Otherwise, a cycle exists, and topological sorting is not possible.
+
+// O(vertices + edges) time
+// O(vertices + edges) space
+function topologicalSort(vertices, edges) {
+    const adjList = new Map();
+    for (let i = 0; i < vertices; i++) adjList.set(i, []);
+    for (const [a, b] of edges) adjList.get(a).push(b);
+
+    const indegrees = new Map();
+    for (let i = 0; i < vertices; i++) indegrees.set(i, 0);
+    for (const [_, neighbors] of adjList) {
+        for (const neighbor of neighbors) {
+            indegrees.set(neighbor, indegrees.get(neighbor) + 1);
         }
     }
 
     const q = [];
-    for (let i = 1; i < indegrees.length; i++) {
-        if (indegrees[i] === 0) q.push(i);
+    for (const [job, deg] of indegrees) {
+        if (deg === 0) q.push(job);
     }
 
     const res = [];
@@ -20,22 +39,14 @@ function topologicalSort(jobs, deps) {
         const cur = q.shift();
         res.push(cur);
 
-        for (const node of adjList.get(cur)) {
-            indegrees[node] -= 1;
-            if (indegrees[node] === 0) q.push(node)
+        for (const neighbor of adjList.get(cur)) {
+            indegrees.set(neighbor, indegrees.get(neighbor) - 1);
+            if (indegrees.get(neighbor) === 0) q.push(neighbor);
         }
     }
 
-    return res;
+    return res.length !== vertices ? [] : res;
 }
 
-console.log(topologicalSort(
-    [1, 2, 3, 4],
-    [
-        [1, 2],
-        [1, 3],
-        [3, 2],
-        [4, 2],
-        [4, 3]
-    ]
-));
+console.log(topologicalSort(4, [[0, 1], [0, 2], [1, 2], [2, 3]]));
+console.log(topologicalSort(3, [[0, 1], [1, 2], [2, 0]])); // cycle
