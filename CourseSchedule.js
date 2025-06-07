5d087affb405de2a87efcc562d10cb22d478bd57
JoyChudasama <joychudasama11@gmail.com>
2025-06-07 07:49:01 -0300

    Course Schedule

--- CourseSchedule.js
+++ CourseSchedule.js
@@ -51,5 +51,39 @@ function canFinish(numCourses, prerequisites) {
     return true;
 }
 
+// Kahn's Algo
+// O(vertex + edge) time
+// O(vertex + edge) space
+function canFinishBFS(vertices, edges) {
+    const adjList = new Map();
+    for (let i = 0; i < vertices; i++) adjList.set(i, []);
+    for (const [a, b] of edges) adjList.get(a).push(b);
 
-console.log(canFinish(2, [[0, 1]]))
+    const indegrees = new Map();
+    for (let i = 0; i < vertices; i++) indegrees.set(i, 0);
+    for (const [_, neighbors] of adjList) {
+        for (const neighbor of neighbors) {
+            indegrees.set(neighbor, indegrees.get(neighbor) + 1);
+        }
+    }
+
+    const q = [];
+    for (const [job, deg] of indegrees) {
+        if (deg === 0) q.push(job);
+    }
+
+    const res = [];
+    while (q.length) {
+        const cur = q.shift();
+        res.push(cur);
+
+        for (const neighbor of adjList.get(cur)) {
+            indegrees.set(neighbor, indegrees.get(neighbor) - 1);
+            if (indegrees.get(neighbor) === 0) q.push(neighbor);
+        }
+    }
+
+    return res.length === vertices;
+}
+
+console.log(canFinishBFS(2, [[0, 1]]))
